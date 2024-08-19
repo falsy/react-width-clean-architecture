1811c49d198333e503016654c3013b3f86509897
CheolJe <me@falsy.me>
2024-08-19 09:10:00 +0900

    feat: Modify presenter function

--- src/adapters/presenters/index.ts
+++ src/adapters/presenters/index.ts
@@ -1,11 +1,15 @@
 import IUseCases from "adapters/domains/useCases/interfaces"
 import UserPresenter from "./UserPresenter"
 import IPresenter from "./interfaces"
-import ToDoPresenter from "./ToDoPresenter"
+import TransactionPresenter from "./TransactionPresenter"
+import CardPresenter from "./CardPresenter"
+import AccountPresenter from "./AccountPresenter"
 
 export default function presenters(useCases: IUseCases): IPresenter {
   return {
     user: new UserPresenter(useCases.user),
-    todo: new ToDoPresenter(useCases.todo)
+    transaction: new TransactionPresenter(useCases.transaction),
+    card: new CardPresenter(useCases.card),
+    account: new AccountPresenter(useCases.account)
   }
 }
